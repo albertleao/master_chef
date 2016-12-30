da7c89dca60d56ff88def060e8ad186753cc45bd
Albert Leao <albertleao@live.com>
2016-12-30 17:40:30 -0500

    added undeploy method ot delete

--- local_cookbooks/homesync-php-app/recipes/deploy.rb
+++ local_cookbooks/homesync-php-app/recipes/deploy.rb
@@ -16,45 +16,47 @@ directory '/srv/www' do
 end
 
 search("aws_opsworks_app").each do |app|
-  #Delete the current project directory
-  directory "/srv/www/#{app['shortname']}" do
-    recursive true
-    action :delete
-  end
-
-  application "/srv/www/#{app['shortname']}" do
-    # Application resource properties.
-    owner 'root'
-    group 'root'
-
-    # Subresources, like normal recipe code.
-    application_git "/srv/www/#{app['shortname']}" do
-      repository app['app_source']['url']
-      deploy_key app['app_source']['ssh_key']
-      revision app['app_source']['revision']
+  if app['deploy'] == true do
+    #Delete the current project directory
+    directory "/srv/www/#{app['shortname']}" do
+      recursive true
+      action :delete
     end
-    
-    web_app "#{app['shortname']}" do
-       server_name "#{app['domains'].first}"
 
-       unless app['domains'][1, app['domains'].size].empty?
-         server_aliases app['domains'][1, app['domains'].size]
-       end
-       docroot "/srv/www/#{app['shortname']}/#{app['attributes']['document_root']}"
-       env_vars app['environment']
-       template 'web_app.conf.erb'
+    application "/srv/www/#{app['shortname']}" do
+      # Application resource properties.
+      owner 'root'
+      group 'root'
 
-       apache_module "mpm-event" do
-        conf false
+      # Subresources, like normal recipe code.
+      application_git "/srv/www/#{app['shortname']}" do
+        repository app['app_source']['url']
+        deploy_key app['app_source']['ssh_key']
+        revision app['app_source']['revision']
       end
-    end
+      
+      web_app "#{app['shortname']}" do
+         server_name "#{app['domains'].first}"
 
-    composer_project "/srv/www/#{app['shortname']}" do
-        dev false
-        quiet true
-        prefer_dist false
-        optimize_autoloader true
-        action :install
+         unless app['domains'][1, app['domains'].size].empty?
+           server_aliases app['domains'][1, app['domains'].size]
+         end
+         docroot "/srv/www/#{app['shortname']}/#{app['attributes']['document_root']}"
+         env_vars app['environment']
+         template 'web_app.conf.erb'
+
+         apache_module "mpm-event" do
+          conf false
+        end
+      end
+
+      composer_project "/srv/www/#{app['shortname']}" do
+          dev false
+          quiet true
+          prefer_dist false
+          optimize_autoloader true
+          action :install
+      end
     end
   end
 end
