5ec9c3cd1e0b7916d6897f512592d105c4afdc79
Albert Leao <albertleao@live.com>
2017-02-02 01:46:04 -0500

    deployment working

--- deploy-master.sh
+++ deploy-master.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+echo 'Packaging cookbooks.'
 berks package my-cookbooks.tar.gz
+echo 'Done Packaging cookbooks.'
 
+echo 'Uploading to S3 Production.'
 aws s3 cp my-cookbooks.tar.gz s3://homesync-general
+echo 'Done uploading to S3.'
+
+rm my-cookbooks.tar.gz
